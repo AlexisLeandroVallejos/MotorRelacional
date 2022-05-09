3e0d8dfaa7ec7c44a486647f5489649fa970f41a
AlexisLeandroVallejos <alexv4lle@gmail.com>
2022-05-09 12:29:50 -0300

    primer commit

---
+++ motorRelacional.sql
@@ -0,0 +1,18 @@
+/* 
+Comandos iniciales, Solo hacer la primera vez al crear
+
+En cmd/terminal/bash:
+Para crear la base de datos world:
+--3.1.1
+createdb -U postgres world
+
+Para agregar el script world.sql a la db:
+--3.1.2
+psql -f "rutaArchivo" -U postgres -d world
+
+*postgres como usuario default/super del postgresql
+*/
+
+--3.1.3
+SELECT * FROM CITY
+
